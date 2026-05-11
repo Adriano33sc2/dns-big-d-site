085354af2e809e4d2e0555fbb14b31ab8d62158c
Fábio Dias Francisco <fabiodiasfrancisco27@gmail.com>
2026-05-11 15:36:29 +0100

    Fixed issues with events/subs Fixed refresh screen fallback

--- backend/src/dns_big_d_site/backend/core.clj
+++ backend/src/dns_big_d_site/backend/core.clj
@@ -1,20 +1,28 @@
 (ns dns-big-d-site.backend.core
-  (:require [compojure.core :refer [routes]]
+  (:require [clojure.java.io :as io]
+            [compojure.core :refer [routes]]
             [compojure.route :as route]
             [dns-big-d-site.backend.db :as db]
             [dns-big-d-site.backend.routes.auth :refer [auth-routes]]
             [dns-big-d-site.backend.routes.build-orders :refer [build-order-routes]]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.cors :refer [wrap-cors]]
-            [ring.middleware.file :refer [wrap-file]]
             [ring.middleware.json :as json]
             [ring.middleware.multipart-params :as multipart]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.resource :refer [wrap-resource]])
   (:gen-class))
 
+(defn- serve-index [request]
+  (when (and (= :get (:request-method request))
+             (not (.startsWith (:uri request) "/api")))
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (slurp (io/resource "index.html"))}))
+
 (def app
-  (-> (routes auth-routes
+  (-> (routes serve-index
+              auth-routes
               build-order-routes
               (route/not-found "Not Found"))
       json/wrap-json-response
@@ -23,8 +31,7 @@
                  :access-control-allow-methods [:get :post :put :delete :options])
       wrap-keyword-params
       (multipart/wrap-multipart-params {:max-file-size (* 1024 1024 50)}); 50MB max
-      (wrap-file "public")
-      (wrap-resource "public")))
+      (wrap-resource "")))
 
 (defn -main [& [port]]
   (let [port (or port 3000)]
